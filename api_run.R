4231af3070525645fb36b03b22f24b9e4194d206
degrootel <degrootel@carnegiemnh.org>
2022-12-06 07:29:25 -0500

    Initial commit

--- api_run.R
+++ api_run.R
@@ -1,22 +1,21 @@
-source("functions/api_postgres.R")
-library(DBI)
+source("data_tools/functions/api_postgres.R")
 start <- Sys.time()
 
-####SETTINGS#####
-my_token <- "your token here"
-db_name <- "mydb"
-myproject <- "CTT Project Name" #this is your project name on your CTT account
-conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
+####SETTINGS####
+outpath <- "~/data/" 
+my_token <- "da0ca3812afa77add5606779823a6b4f3c657df9196e725f540b303ec50fa979"
+db_name <- "parcstopover"
+myproject <- "PARC Stopover" #this is your project name on your CTT account
+#conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
 ################
 
-get_my_data(my_token, "~/Documents/data/radio_projects/myproject" , conn, myproject) #the folder path is where you want your downloaded files to go
-outpath <- "~/Documents/data/radio_projects/myproject"
-update_db(conn, outpath, myproject)
-dbDisconnect(conn)
+get_my_data(my_token,"/data") # conn, myproject)
+
+#update_db(conn, outpath, myproject)
+#dbDisconnect(conn)
 
 #source("functions/filecatch.R")
 #findfiles(outpath, "directory path where you want your caught files to go")
 
 time_elapse <- Sys.time() - start
 print(time_elapse)
-
