8617ba5c7578639a4f5595023f1a5682c0f534b8
degrootel <degrootel@carnegiemnh.org>
2022-12-07 12:42:15 -0500

    update api run

--- api_run.R
+++ api_run.R
@@ -1,15 +1,15 @@
-source("data_tools/functions/api_postgres.R")
+source("C:/Users/degrootel/Documents/Parc Stopover/data_tools/functions/api_postgres.R")
 start <- Sys.time()
 
 ####SETTINGS####
-outpath <- "~/data/" 
+outpath <- "C:/Users/degrootel/Documents/Parc Stopover/data" 
 my_token <- "da0ca3812afa77add5606779823a6b4f3c657df9196e725f540b303ec50fa979"
 db_name <- "parcstopover"
 myproject <- "PARC Stopover" #this is your project name on your CTT account
 #conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
 ################
 
-get_my_data(my_token,"/data") # conn, myproject)
+get_my_data(my_token,"C:/Users/degrootel/Documents/Parc Stopover/data") # conn, myproject)
 
 #update_db(conn, outpath, myproject)
 #dbDisconnect(conn)
